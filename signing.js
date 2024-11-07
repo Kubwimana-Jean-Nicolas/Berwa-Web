4a2ebc5a80cd896ab5874f0d7a26e3331850664a
kubwimana Jean nicolas <kunicolas2018@gmail.com>
2024-11-08 00:23:22 +0200

    index rename

---
+++ signing.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+const hamburgerMenu = document.querySelector(" .hamburger");
+const mobileNav =
+console.log("hamburgerMenu");
+
+hamburgerMenu.addEventListener("click", doSomething);
+
+function doSomething () {
+    console.log("hamburger Menu has been clicked");
+}
+
